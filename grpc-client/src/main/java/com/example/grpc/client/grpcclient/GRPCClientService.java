8c389760384b28d56b785c1d6651c1b1fd364baa
aogunwoolu <a.ogunwoolu@outlook.com>
2022-03-16 11:04:15 +0000

    array changes

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -11,8 +11,35 @@ import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Tuple<X, Y> { 
+	public final X x; 
+	public final Y y; 
+	public Tuple(X x, Y y) { 
+	  this.x = x; 
+	  this.y = y; 
+	} 
+  } 
+
 @Service
 public class GRPCClientService {
+	ManagedChannel channel1 = ManagedChannelBuilder.forAddress("35.225.79.176", 8080) .usePlaintext().build();
+	ManagedChannel channel2 = ManagedChannelBuilder.forAddress("34.123.43.111", 8080) .usePlaintext().build();
+	ManagedChannel channel3 = ManagedChannelBuilder.forAddress("35.239.236.128", 8080) .usePlaintext().build();
+
+	MatrixServiceGrpc.MatrixServiceBlockingStub stub1 = MatrixServiceGrpc.newBlockingStub(channel1);
+	MatrixServiceGrpc.MatrixServiceBlockingStub stub2 = MatrixServiceGrpc.newBlockingStub(channel2);
+	MatrixServiceGrpc.MatrixServiceBlockingStub stub3 = MatrixServiceGrpc.newBlockingStub(channel3);
+
+	ArrayList<Tuple<ManagedChannel, MatrixServiceGrpc.MatrixServiceBlockingStub>> Servers = Arrays.asList(
+		new Tuple<ManagedChannel, MatrixServiceGrpc.MatrixServiceBlockingStub>(channel1, stub1),
+		new Tuple<ManagedChannel, MatrixServiceGrpc.MatrixServiceBlockingStub>(channel2, stub2),
+		new Tuple<ManagedChannel, MatrixServiceGrpc.MatrixServiceBlockingStub>(channel3, stub3)
+	);
+
     public String ping() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
         .usePlaintext()
