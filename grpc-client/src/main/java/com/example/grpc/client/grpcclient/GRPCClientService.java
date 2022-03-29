65de22db8027927524f42fb0851b3054470ce0e0
Abi <a.ogunwoolu@outlook.com>
2022-03-29 14:41:58 +0100

    client error changes

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -22,26 +22,30 @@ import java.util.Map;
 @Service
 public class GRPCClientService {
 
-	List<ManagedChannelBuilder> channels = Arrays.asList(
-		ManagedChannelBuilder.forAddress("35.225.79.176", 8080),
-		ManagedChannelBuilder.forAddress("34.123.43.111", 8080),
-		ManagedChannelBuilder.forAddress("35.239.236.128", 8080)
-	);
+	// List<ManagedChannelBuilder> channels = Arrays.asList(
+	// 	ManagedChannelBuilder.forAddress("35.225.79.176", 8080),
+	// 	ManagedChannelBuilder.forAddress("34.123.43.111", 8080),
+	// 	ManagedChannelBuilder.forAddress("35.239.236.128", 8080)
+	// );
 
-	List<MatrixServiceGrpc.MatrixServiceBlockingStub> stubs = Arrays.asList(
-		MatrixServiceGrpc.newBlockingStub(channels.get(0)),
-		MatrixServiceGrpc.newBlockingStub(channels.get(1)),
-		MatrixServiceGrpc.newBlockingStub(channels.get(2))
-	);
+	// List<MatrixServiceGrpc.MatrixServiceBlockingStub> stubs = Arrays.asList(
+	// 	MatrixServiceGrpc.newBlockingStub(channels.get(0)),
+	// 	MatrixServiceGrpc.newBlockingStub(channels.get(1)),
+	// 	MatrixServiceGrpc.newBlockingStub(channels.get(2))
+	// );
 
 	int currentServer = ThreadLocalRandom.current().nextInt(0, channels.size());
 
     public String ping() {
-        ManagedChannel channel = channels.get(currentServer)
-        .usePlaintext()
-        .build(); 
+        // ManagedChannel channel = channels.get(currentServer)
+        // .usePlaintext()
+        // .build(); 
 
-		PingPongServiceGrpc.PingPongServiceBlockingStub stub = stubs.get(currentServer);        
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
+		.usePlaintext()
+		.build();
+
+		PingPongServiceGrpc.PingPongServiceBlockingStub stub = stubs.get(channel);        
 		PongResponse helloResponse = stub.ping(PingRequest.newBuilder()
         .setPing("")
         .build()); 
@@ -90,8 +94,8 @@ public class GRPCClientService {
 		// 	.setB11(6)
 		// 	.build());
 			
-		String resp= "<table><tr><td>"+ A.getC00()+"</td><td>"+A.getC01()+"</td><tr><td>"+A.getC10()+"</td><td>"+A.getC11()+"<td></tr></table>\n";
+		//String resp= "<table><tr><td>"+ A.getC00()+"</td><td>"+A.getC01()+"</td><tr><td>"+A.getC10()+"</td><td>"+A.getC11()+"<td></tr></table>\n";
 		//resp += "<br/><form method='POST' action='/upload' enctype='multipart/form-data'><input type='file' name='file' /><br/><br/><input type='submit' value='Submit' /></form>";
-		return resp;
+		return "";
     }
 }
