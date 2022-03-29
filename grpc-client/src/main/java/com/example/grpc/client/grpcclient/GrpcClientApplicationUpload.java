e84b37a7e878e28d61636981018dd33b5b2f90e3
Abi <a.ogunwoolu@outlook.com>
2022-03-29 15:18:38 +0100

    mapping removal

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplicationUpload.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplicationUpload.java
@@ -18,40 +18,40 @@ public class GrpcClientApplicationUpload {
     //Save the uploaded file to this folder
     private static String UPLOADED_FOLDER = "F://temp//";
 
-    @GetMapping("/")
-    public String index() {
-        return "upload";
-    }
+    // @GetMapping("/")
+    // public String index() {
+    //     return "upload";
+    // }
 
-    @PostMapping("/upload") // //new annotation since 4.3
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
-        RedirectAttributes redirectAttributes) {
+    // @PostMapping("/upload") // //new annotation since 4.3
+    // public String singleFileUpload(@RequestParam("file") MultipartFile file,
+    //     RedirectAttributes redirectAttributes) {
 
-        if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-            return "redirect:uploadStatus";
-        }
+    //     if (file.isEmpty()) {
+    //         redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+    //         return "redirect:uploadStatus";
+    //     }
 
-        try {
+    //     try {
 
-            // Get the file and save it somewhere
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
+    //         // Get the file and save it somewhere
+    //         byte[] bytes = file.getBytes();
+    //         Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+    //         Files.write(path, bytes);
 
-            redirectAttributes.addFlashAttribute("message",
-                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+    //         redirectAttributes.addFlashAttribute("message",
+    //                 "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
 
-        return "redirect:/uploadStatus";
-    }
+    //     return "redirect:/uploadStatus";
+    // }
 
-    @GetMapping("/uploadStatus")
-    public String uploadStatus() {
-        return "uploadStatus";
-    }
+    // @GetMapping("/uploadStatus")
+    // public String uploadStatus() {
+    //     return "uploadStatus";
+    // }
 
 }
